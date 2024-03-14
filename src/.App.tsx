581724f9d4ed080ff940e60dd72fc86586a2aaba
leevayy <maoaoa7@gmail.com>
2024-03-14 17:52:50 +0300

    make base for refactoring

---
+++ src/.App.tsx
@@ -0,0 +1,72 @@
+// import type { KanbanBoard } from "./types";
+// import { ReactComponent as MenuIcon } from "./icons/menu.svg";
+// import { ReactComponent as UserIcon } from "./icons/user-icon.svg";
+// import { ReactComponent as CalendarIcon } from "./icons/calendar-icon.svg";
+// import { Menu, MenuItem } from "./components/menu/menu";
+// import { Board } from "./components/board/board";
+// import { Calendar } from "./components/calendar/calendar";
+
+// const board: KanbanBoard = {
+// 	name: "Some longer header to test this out",
+// 	lists: [
+// 		{
+// 			name: "Hello!",
+// 			position: 0,
+// 			cards: [
+// 				{
+// 					text: "what's up. if i add a bunch of text it will result in longer card, right? it must",
+// 					position: 0,
+// 					id: "cueo"
+// 				}
+// 			],
+// 			id: "x9rh"
+// 		},
+// 		{
+// 			name: "hola",
+// 			position: 1,
+// 			cards: [
+// 				{
+// 					text: "amigo",
+// 					position: 0,
+// 					id: "qclh"
+// 				},
+// 				{
+// 					text: "el gato",
+// 					position: 1,
+// 					id: "0p16"
+// 				},
+// 				{
+// 					text: "las ninas estoy un poco loco",
+// 					position: 2,
+// 					id: "z85y"
+// 				}
+// 			],
+// 			id: "niq4"
+// 		},
+// 		{
+// 			name: "Привет!",
+// 			position: 2,
+// 			cards: [],
+// 			id: "ve9l"
+// 		}
+// 	],
+// 	id: "vfr8"
+// };
+
+// function App() {
+// 	return (
+// 		<>
+// 			<Menu items={[
+// 				{name: "menu", icon: MenuIcon},
+// 				{name: "calendar", icon: CalendarIcon},
+// 				{name: "user-icon", icon: UserIcon,  align: "right"},
+// 				{name: "board-name", text: board.name, align: "center"}
+// 			]}>
+// 			</Menu>
+// 			<Board board={board} />
+// 			{/* <Calendar /> */}
+// 		</>
+// 	);
+// }
+
+// export default App;
---
+++ src/components/utils/.textInput.tsx
@@ -0,0 +1,50 @@
+// import React, { forwardRef, useRef } from "react";
+
+// type InputLikeProps = React.ComponentPropsWithRef<"input">;
+
+// const TextInputLike = forwardRef(function TextInputLike(
+// 	props: InputLikeProps,
+// 	ref: React.Ref<HTMLInputElement>
+// ) {
+// 	return (
+// 		<div 
+// 			{...props} ref={ref} 
+// 			className={`${props.className} input-like`}
+// 			contentEditable
+// 		>
+// 			{props.value}
+// 		</div>
+// 	);
+// });
+
+// type TextInputProps = React.ComponentPropsWithRef<"input"> & {
+// 	setInputText: (state: string) => void;
+// 	inputType: "input" | "input-like";
+// }
+
+// export function TextInput(props: TextInputProps) {
+// 	const setInputText = props.setInputText;
+
+// 	const inputRef = useRef<HTMLInputElement>(null);
+
+// 	if (props.inputType === "input") {
+// 		return (
+// 			<input
+// 				{...props}
+// 				ref={inputRef}
+// 				className={`${props.className} focusable-input`}
+// 				onInput={() => setInputText(inputRef.current!.value)}
+// 			/>
+// 		);
+// 	}
+// 	if (props.inputType === "input-like") {
+// 		return (
+// 			<TextInputLike
+// 				{...props}
+// 				ref={inputRef}
+// 				className={`${props.className} focusable-input`}
+// 				onInput={() => setInputText(inputRef.current!.value)}
+// 			/>
+// 		);
+// 	}
+// }
