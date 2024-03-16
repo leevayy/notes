de57113a5dd8ba351841346a0149cbf4d87989c0
leevayy <maoaoa7@gmail.com>
2024-03-16 12:34:53 +0300

    fix textarea caret

--- src/components/utils/EditableText/EditableText.tsx
+++ src/components/utils/EditableText/EditableText.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useUnit } from "effector-react";
 import { $inEditModeId, editableTextClicked, editableTextBlured } from "./model";
 import styles from "./EditableText.module.css";
@@ -24,13 +24,19 @@ export function EditableText({ value, id }: EditableTextProps) {
 		}
 	}, [value, textareaRef]);
 
+	useEffect(() => {
+		if (textareaRef !== null) {
+			textareaRef.setSelectionRange(value.length, value.length);
+		}
+	}, [textareaRef]);
+
 	const isInEditMode = inEditModeId === id;
 
 	return (
 		<div className={styles.editable_text} onClick={() => handleEditableTextClicked(id)}>
 			{isInEditMode ? (
 				<textarea 
-					autoFocus={true} 
+					autoFocus={true}
 					onBlur={handleEditableTextBlured} 
 					defaultValue={value} 
 					ref={(el) => setTextareaRef(el)} 
