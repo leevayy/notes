9e8a035d83438c769845a5f6564f2324feb2472b
leevayy <maoaoa7@gmail.com>
2024-03-24 12:04:17 +0300

    refactor List/getCardDropPosition

--- src/components/board/List/List.tsx
+++ src/components/board/List/List.tsx
@@ -4,14 +4,13 @@ import { EditableText } from "../../utils/EditableText/EditableText";
 import styles from "./List.module.css";
 import MakeNewCard from "./MakeNewCard/MakeNewCard";
 import { cardInserted } from "./model";
-import {$draggedCard, cardRemoved } from "../model";
+import { $draggedCard, cardRemoved } from "../model";
 import { getId } from "../../../utils/utils";
 
 type ListProps = React.PropsWithChildren & {
 	list: KanbanList;
-	setDropPosition: React.Dispatch<React.SetStateAction<number>>
+	setDropPosition: React.Dispatch<React.SetStateAction<number>>;
 };
-
 
 // bad code, but i can't figure out how to calculate it on the fly
 // needs to be readjasted with each design change
@@ -21,33 +20,43 @@ const getCardDropPosition = (dragEvent: React.DragEvent<HTMLLIElement>) => {
 	const cards = dragEvent.currentTarget.children.item(1);
 
 	if (!cards) {
-		throw new Error("Droped card to list which has no cards!");
+		throw new Error("Droped card to list which has no cards wrapper!");
 	}
 
 	const cardsArr = Array.from(cards.children);
 
-	const cardsTopScreenOffset = cards.getBoundingClientRect().top;
-	const windowScroll = window.scrollY;
-	const topOffset = cardsTopScreenOffset - windowScroll;
+	const calculateDropYWithOffset = (dropY: number, cards: Element) => {
+		const cardsTopScreenOffset = cards.getBoundingClientRect().top;
+		const windowScroll = window.scrollY;
+		const topOffset = cardsTopScreenOffset - windowScroll;
+
+		return dropY - topOffset
+	}
+
+	const dropY = calculateDropYWithOffset(dragEvent.clientY, cards);
+
+	const calculateGap = (cardsArr: Element[], cardsHeight: number) => {
+		if (cardsArr.length <= 1) {
+			return 0;
+		}
 	
-	const dropY = dragEvent.clientY - topOffset;
+		const nonCardSpace = cardsArr.reduce((nonCardSpace, card) => {
+			return nonCardSpace - card.clientHeight;
+		}, cardsHeight);
 
-	const cardsHeight = cards.clientHeight;
+		return (nonCardSpace - 2 * CARDS_PADDING) / (cardsArr.length - 1)
+	}
 
-	const nonCardSpace = cardsArr.reduce((nonCardSpace, card) => {
-		return nonCardSpace - card.clientHeight;
-	}, cardsHeight);
+	const gap = calculateGap(cardsArr, cards.clientHeight);
 
-	const gap = cardsArr.length > 1 ? (nonCardSpace - 2 * CARDS_PADDING) / (cardsArr.length - 1) : 0;
-	
-	let offsetY = CARDS_PADDING;
+	let currentInsertionY = CARDS_PADDING;
 	for (const [cardIndex, card] of cardsArr.entries()) {
-		const nextOffset = offsetY + card.clientHeight +
-			(cardIndex !== 0 ? gap : 0);
+		const nextInsertionY = currentInsertionY + card.clientHeight + (cardIndex !== 0 ? gap : 0);
 
-		const droppedBeforeNextOffset = nextOffset > dropY;
+		const droppedBeforeNextOffset = nextInsertionY > dropY;
+		
 		if (droppedBeforeNextOffset) {
-			const droppedInFirstHalfOfCard = dropY > offsetY + gap + card.clientHeight / 2;
+			const droppedInFirstHalfOfCard = dropY > currentInsertionY + gap + card.clientHeight / 2;
 
 			if (droppedInFirstHalfOfCard) {
 				return cardIndex + 1;
@@ -55,28 +64,29 @@ const getCardDropPosition = (dragEvent: React.DragEvent<HTMLLIElement>) => {
 				return cardIndex;
 			}
 		}
-		offsetY = nextOffset;
+		currentInsertionY = nextInsertionY;
 	}
 
 	return cardsArr.length;
-}
+};
 
 export default function List({ children, list, setDropPosition }: ListProps) {
 	const draggedCard = useUnit($draggedCard);
 	const insertCard = useUnit(cardInserted);
 	const removeCard = useUnit(cardRemoved);
 
-	const unshiftCard = (newCard: KanbanCard) => insertCard({
-		card: newCard, 
-		updatedList: list, 
-		insertionIndex: 0
-	});
+	const unshiftCard = (newCard: KanbanCard) =>
+		insertCard({
+			card: newCard,
+			updatedList: list,
+			insertionIndex: 0,
+		});
 
 	function dragOverHandler(e: React.DragEvent<HTMLLIElement>) {
 		e.preventDefault();
 
 		if (!draggedCard) {
-			throw new Error('Dragged card is null');
+			throw new Error("Dragged card is null");
 		}
 
 		const cardDropPosition = getCardDropPosition(e);
@@ -87,15 +97,15 @@ export default function List({ children, list, setDropPosition }: ListProps) {
 		e.preventDefault();
 
 		if (!draggedCard) {
-			throw new Error('Dragged card is null');
+			throw new Error("Dragged card is null");
 		}
 
 		const cardDropPosition = getCardDropPosition(e);
 
 		insertCard({
-			card: {...draggedCard, id: getId(), position: cardDropPosition}, 
+			card: { ...draggedCard, id: getId(), position: cardDropPosition },
 			updatedList: list,
-			insertionIndex: cardDropPosition
+			insertionIndex: cardDropPosition,
 		});
 
 		removeCard(draggedCard!["id"]);
@@ -104,16 +114,10 @@ export default function List({ children, list, setDropPosition }: ListProps) {
 	}
 
 	return (
-		<li className={styles.list}
-			onDragOver={dragOverHandler}
-			onDrop={dropHandler}
-		>
-			<ListHeader
-				listName={list.name}
-				id={list.id}
-			/>
+		<li className={styles.list} onDragOver={dragOverHandler} onDrop={dropHandler}>
+			<ListHeader listName={list.name} id={list.id} />
 			<ul className={styles.list_cards_wrapper}>{children}</ul>
-			<MakeNewCard unshiftCard={unshiftCard}/>
+			<MakeNewCard unshiftCard={unshiftCard} />
 		</li>
 	);
 }
@@ -122,13 +126,17 @@ type ListHeaderProps = {
 	listName: string;
 	id: string;
 };
-  
-function ListHeader({listName, id}: ListHeaderProps) {
-	return <h2 className={styles.list_name_header}>
-		<EditableText 
-			value={listName}
-			id={id}
-			onChange={(e) => {console.log("onChange for ListHeader wasn't implemented yet")}}
-		/>
-	</h2>
+
+function ListHeader({ listName, id }: ListHeaderProps) {
+	return (
+		<h2 className={styles.list_name_header}>
+			<EditableText
+				value={listName}
+				id={id}
+				onChange={(e) => {
+					console.log("onChange for ListHeader wasn't implemented yet");
+				}}
+			/>
+		</h2>
+	);
 }
