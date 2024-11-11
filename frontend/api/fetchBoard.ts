ed8ee5d65ebea04be71b314200eced6e50ce41ea
leevayy <maoaoa7@gmail.com>
2024-11-12 01:59:13 +0300

    feat: updated frontend to use my backend for fetchBoard

--- frontend/api/fetchBoard.ts
+++ frontend/api/fetchBoard.ts
@@ -1,50 +1,7 @@
 export default async function fetchBoard() {
-    await new Promise((res) => setTimeout(res, 2000));
+    const res = await fetch('http://localhost:8000/board');
+
+    const board = await res.json();
     
-return {
-        name: "Some longer header to test this out",
-        lists: [
-            {
-                name: "Hello!",
-                position: 0,
-                cards: [
-                    {
-                        text: "what's up. if i add a bunch of text it will result in longer card, right? it must",
-                        position: 0,
-                        id: "cueo"
-                    }
-                ],
-                id: "x9rh"
-            },
-            {
-                name: "hola",
-                position: 1,
-                cards: [
-                    {
-                        text: "amigo",
-                        position: 0,
-                        id: "qclh"
-                    },
-                    {
-                        text: "el gato",
-                        position: 1,
-                        id: "0p16"
-                    },
-                    {
-                        text: "las ninas estoy un poco loco",
-                        position: 2,
-                        id: "z85y"
-                    }
-                ],
-                id: "niq4"
-            },
-            {
-                name: "Привет!",
-                position: 2,
-                cards: [],
-                id: "ve9l"
-            }
-        ],
-        id: "vfr8"
-    };
+    return board;
 }
